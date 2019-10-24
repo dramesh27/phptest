4340b9c1cb24f47de633216e3d7327753edf13bb
dramesh27 <41628886+dramesh27@users.noreply.github.com>
2019-10-24 17:08:01 +0200

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,11 +1,11 @@
 <?php
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbhost = getenv("PS_SERVICE_HOST");
+$dbport = getenv("PS_SERVICE_PORT");
 $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+$connection =  pg_connect($dbhost, $dbport, $dbname, $dbuser, $dbpwd);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
